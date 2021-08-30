56add882e9391ee709b0acf73f11102eb5179aac
mrugesh <mrugesh.kshatriya@outlook.com>
2021-08-30 10:50:02 +0530

    Changes

--- database/migrations/2021_08_30_050546_get_post_procedure.php
+++ database/migrations/2021_08_30_050546_get_post_procedure.php
@@ -19,7 +19,7 @@ class GetPostProcedure extends Migration
         BEGIN
         SELECT posts.id,posts.post,posts.is_repost,posts.type,users.id as user,users.name,users.email, count(distinct comments.id) as comment_count,sum(case when reacts.type = 1 then 1 else 0 end) as likes,sum(case when reacts.type = 2 then 1 else 0 end) as bookmarks,count(DISTINCT likes.id) as user_liked,
         count(DISTINCT bookmark.id) as user_bookmark  FROM `posts`  join users on users.id=posts.user_id  left join comments on comments.post_id=posts.id left join reacts on reacts.post_id=posts.id left join reacts likes on likes.post_id=posts.id and likes.user_id=user_id and likes.type=1 
-        left join reacts bookmark on bookmark.post_id=posts.id and bookmark.user_id=user_id and bookmark.type=2  WHERE posts.id=post_id;
+        left join reacts bookmark on bookmark.post_id=posts.id and bookmark.user_id=user_id and bookmark.type=2  WHERE posts.id=post_id group by posts.id;
         END
         ";
         
